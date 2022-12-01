0685e4f66fa3324fb181454683d3355a0cafec45
Ufuk Filiz <92686275+DevFilizUfuk@users.noreply.github.com>
2022-12-01 09:22:01 +0100

    Update Devoir_SNT_Python_FILIZ_Ufuk_2nd3.py

--- Devoir_SNT_Python_FILIZ_Ufuk_2nd3.py
+++ Devoir_SNT_Python_FILIZ_Ufuk_2nd3.py
@@ -26,8 +26,5 @@ for y in range (hauteur_image2):
    for x in range (largeur_image2):
 
        r,v,b=image2.getpixel((x,y)) 
-       n_r=b
-       n_v=r
-       n_b=v
-       image2.putpixel((x,y),(n_r,n_v,n_b))
+       image2.putpixel((x,y),(255,145,b))
 image2.show()
